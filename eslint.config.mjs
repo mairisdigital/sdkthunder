a7911ac5cd38094d39d74b54ffce9cc59b420c60
mairisdev <mairisdigital@icloud.com>
2025-08-07 12:34:24 +0300

    FixedNextConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "jsx-a11y/alt-text": "off",
+    }
+  }
 ];
 
 export default eslintConfig;
